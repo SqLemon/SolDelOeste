6b95b1953fa9d69c18efe73a84ffc72219849286
damiMela <damimelamed@gmail.com>
2018-01-26 16:56:41 -0300

    agregue un comentariollamado hola, esta tu vieja?

--- app/src/main/java/melamed/soldeloesteapp/GetProductosClass.java
+++ app/src/main/java/melamed/soldeloesteapp/GetProductosClass.java
@@ -14,6 +14,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 class GetProductosClass extends AsyncTask<Void, Void, ProductList> {
+    /// hola
     private URL url;
     private onTaskCompletedListener listener;
 
